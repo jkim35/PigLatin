678cf5741485541c63a4e2bc8d97e67eeb0ca80f
kcavs <159186856+kcavs@users.noreply.github.com>
2024-09-26 15:00:20 -0400

    hi

--- pigLatin.java
+++ pigLatin.java
@@ -32,6 +32,32 @@ class PigLatin
 		n=(n+(s.substring(0,1))+"ay");
 		return n;
 	}
+	public static String startsTwoConsonant(String y){
+		boolean k = isVowel(y);
+		String s=y.substring(1,2);
+		boolean x=false;
+		String n = y;
+		if((s.equals("a"))||(s.equals("A"))){
+			x=true;
+		}
+		if((s.equals("e"))||(s.equals("E"))){
+			x=true;
+		}
+		if((s.equals("i"))||(s.equals("I"))){
+			x=true;
+		}
+		if((s.equals("o"))||(s.equals("O"))){
+			x=true;
+		}
+		if((s.equals("u"))||(s.equals("U"))){
+			x=true;
+		}
+		if((x==false) && (k == false)){
+			n=y.substring(2);
+			n=(n+(s.substring(0,2))+"ay");
+		}
+		return n;
+	}
 	/*()
 	 * Precondition:  s is a String containing a single word.
 	 * Postcondition: returns a word converted into Pig Latin according to the appropriate rule.
@@ -44,9 +70,26 @@ class PigLatin
 			x=startsVowel(s);
 		}
 		if((isVowel(s)==false)){
-			x=startsConsonant(s);
+			String h=startsTwoConsonant(s);
+			if(h.equals(s)){
+				x=startsConsonant(s);
+			}
+			else{
+				x=startsTwoConsonant(s);
+			}
+			
+			
 		}
 		return x;
 	}  
+	public static void main (String[] args)
+	{
+      	Scanner input = new Scanner(System.in);
+		System.out.print("Enter a string: ");
+		String str = input.nextLine();	
+		String piggie = PigLatin.convertWord(str);
+		System.out.println();
+		System.out.println("Converted word: " + piggie);
+	}
    
 }
